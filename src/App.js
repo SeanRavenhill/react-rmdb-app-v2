475f6ab8632176d8b7558c4fde05af3696625808
SeanRavenhill <seanravenhill@gmail.com>
2021-09-21 23:48:37 +0200

    Prettier Autoformating and Scripted Custom Hook
    Prettier Autoformatting across all src files and wrote hook to Fetch data from the API for Home Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-// Components 
-import Header from './components/Header';
-import Home from './components/Home';
+// Components
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 // Styles
 import { GlobalStyle } from "./GlobalStyle";
