379c5c9103531d3053817c7f037bc7ff2bbd79a6
SeanRavenhill <seanravenhill@gmail.com>
2021-09-22 22:43:29 +0200

    Created SearchBar Component Folder
    Created index.js & SearchBar.styles.js. Scaffolded out script for both and created index.js as a controlled component. Imported component into Home.js and wired up. In useHomeFetch.js created a useState hook for index.js (SearchBar Component) and added that to the return.

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -16,9 +17,7 @@ import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-	const { state, load, error } = useHomeFetch();
-
-	console.log(state);
+	const { state, load, error, setSearchTerm } = useHomeFetch();
 
 	return (
 		<React.Fragment>
@@ -29,6 +28,7 @@ const Home = () => {
 					text={state.results[0].overview}
 				/>
 			) : null}
+			<SearchBar setSearchTerm={setSearchTerm}/>
 			<Grid header="Popular Movies">
 				{state.results.map((movie) => (
 					<Thumb
--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,3 +1,4 @@
+// Styles *NB no need for React import here. Component is a simple spinner.
 import { Spinner } from "./Spinner.styles";
 
 export default Spinner;
