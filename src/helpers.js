216e72202161a7a725c196a1277abb41f8851ae1
SeanRavenhill <seanravenhill@gmail.com>
2021-09-24 16:40:57 +0200

    Added sessionstage logic to the App.

--- src/helpers.js
+++ src/helpers.js
@@ -13,3 +13,8 @@ export const convertMoney = (money) => {
 	});
 	return formatter.format(money);
 };
+// Session Storage
+export const isPersistedState = stateName => {
+	const sessionState = sessionStorage.getItem(stateName)
+	return sessionState && JSON.parse(sessionState);
+}
